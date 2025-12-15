c3cb645d97a7f8139897d7416e71603915f94537
나경원 <nkw601@naver.com>
2025-12-16 03:00:39 +0900

    Feat: 회원 탈퇴 프론트 연결

--- src/main/java/com/ssafy/project/api/v1/user/service/UserServiceImpl.java
+++ src/main/java/com/ssafy/project/api/v1/user/service/UserServiceImpl.java
@@ -39,7 +39,7 @@ public class UserServiceImpl implements UserService {
 	public UserDto signup(UserSignupRequest req) {
 		// 비밀번호 일치 여부 확인
 		if(!req.getPassword().equals(req.getPasswordConfirm())) {
-			return null; // 나중에 error로 바꾸기
+			throw new IllegalArgumentException("PASSWORD_MISMATCH");
 		}
 		
 		String hashedPw = passwordEncoder.encode(req.getPassword());
@@ -60,7 +60,7 @@ public class UserServiceImpl implements UserService {
 	        uMapper.insertUser(user);
 	    } catch (DuplicateKeyException e) {
 	        // 중복 관련 예외 → 프론트에게 전달
-	       return null; // 나중에 에러로 바꾸기
+	    	throw new IllegalStateException("DUPLICATE_USER");
 	    }
 		
 		return user;

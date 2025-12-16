b55cf4e0ffc4e95bbe1b50bfdc46ec6eea5c8005
나경원 <nkw601@naver.com>
2025-12-17 03:47:54 +0900

    Fix: blockService bean  등록

--- src/main/java/com/ssafy/project/api/v1/block/service/BlockServiceImpl.java
+++ src/main/java/com/ssafy/project/api/v1/block/service/BlockServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ssafy.project.api.v1.block.service;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -9,6 +10,10 @@ import com.ssafy.project.api.v1.follow.mapper.FollowMapper;
 import com.ssafy.project.api.v1.user.dto.UserDto;
 import com.ssafy.project.api.v1.user.mapper.UserMapper;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
 public class BlockServiceImpl implements BlockService {
 	
 	private final FollowMapper followMapper;

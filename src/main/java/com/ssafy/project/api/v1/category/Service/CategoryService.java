ccfd5342ff50a7af7100b1c5067148bbbc889cee
나경원 <nkw601@naver.com>
2025-12-18 02:19:23 +0900

    Feat: category 조회 service 구현

--- src/main/java/com/ssafy/project/api/v1/category/Service/CategoryService.java
+++ src/main/java/com/ssafy/project/api/v1/category/Service/CategoryService.java
@@ -1,5 +1,9 @@
 package com.ssafy.project.api.v1.category.Service;
 
+import com.ssafy.project.api.v1.category.dto.CategoryListResponse;
+
 public interface CategoryService {
+
+	CategoryListResponse getCategories();
 
 }
--- src/main/java/com/ssafy/project/api/v1/category/Service/CategoryServiceImpl.java
+++ src/main/java/com/ssafy/project/api/v1/category/Service/CategoryServiceImpl.java
@@ -1,8 +1,30 @@
 package com.ssafy.project.api.v1.category.Service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ssafy.project.api.v1.category.dto.CategoryItem;
+import com.ssafy.project.api.v1.category.dto.CategoryListResponse;
+import com.ssafy.project.api.v1.category.mapper.CategoryMapper;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
+    
+	private final CategoryMapper categoryMapper;
+	
+	public CategoryServiceImpl(CategoryMapper categoryMapper) {
+		this.categoryMapper = categoryMapper;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public CategoryListResponse getCategories() {
+		
+		List<CategoryItem> list = categoryMapper.selectAll();
+		
+        return new CategoryListResponse(list.size(), list);
+	}
 
 }

be2da9d70637f65ca640f9c77ddd02a4d249ed8b
나경원 <nkw601@naver.com>
2025-12-18 03:14:52 +0900

    Feat: transcation post mapper 구현

--- src/main/java/com/ssafy/project/api/v1/transaction/dto/TransactionCreateResponse.java
+++ src/main/java/com/ssafy/project/api/v1/transaction/dto/TransactionCreateResponse.java
@@ -9,11 +9,12 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
-@Builder
 @NoArgsConstructor
+@Setter
 public class TransactionCreateResponse {
 	private Long transactionId;
 
